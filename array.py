7c258c1ac8f2ce00dbb476515e771b2dcd3373b6
marcoBelt99 <74368037+marcoBelt99@users.noreply.github.com>
2021-01-22 01:11:17 +0100

    Delete array.py

--- array.py
+++
@@ -1,9 +0,0 @@
-# Definisco un array di qualche elemento
-A = [3, 7, 10, 12, 14, 15, 16, 17, 19, 20]
-print(A)  # Stampo l'array
-for i in range(len(A)):
-    if(A[i] < A[i+1]):  # voglio che sia vera questa proprietà
-        continue
-    else:
-        print("Proprietà non rispettata per: ", A[i])
-        exit(1)
